57991a948a766ecf4d00ba12a6a35fecff869996
Kutluhan <kutluhan.ozbakan21@gmail.com>
2023-09-24 15:20:59 +0300

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import bg from "../assets/bg.jpg";
+import { Link } from 'react-router-dom';
+import "../styles/Home.css"
 function Home() {
   return (
-    <div>Home</div>
+    <div className='mainPage' style={{
+      backgroundImage: `url(${bg})`
+     }}>
+           <div className='whoami' >
+            <Link to="/menu">
+              <button>Kimim Ben</button>
+            </Link>
+           </div>
+    </div>
   )
 }
 
